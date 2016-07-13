9f9d9250f2192bdd90207cee7d4b2f4ff4a3d957
AnghelLeonard <leoprivacy@yahoo.com>
2016-07-13 16:01:05 +0300

    Spring singleton scope

---
+++ SpringSingletonScope/src/main/java/org/spring/hello/MainApp.java
@@ -0,0 +1,26 @@
+package org.spring.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+
+        obj.setName("Alin");
+        obj.printHello();
+
+        HelloWorld obj2 = (HelloWorld) context.getBean("helloBean");
+        obj2.printHello();
+    }
+
+}
