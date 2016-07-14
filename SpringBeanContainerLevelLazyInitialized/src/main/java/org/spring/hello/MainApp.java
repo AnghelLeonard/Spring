6ddeceabc17a9f71e57541a5e57779ba3a368cd1
AnghelLeonard <leoprivacy@yahoo.com>
2016-07-14 14:04:16 +0300

    Spring bean container level lazy initialized

---
+++ SpringBeanContainerLevelLazyInitialized/src/main/java/org/spring/hello/MainApp.java
@@ -0,0 +1,23 @@
+package org.spring.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        /*
+         * ApplicationContext implementations eagerly create and configure all singleton beans as part of
+         * the initialization process
+         */
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+
+        // Output: NOOP
+    }
+
+}
