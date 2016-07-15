2ccc1d2fc605e15f887366336309428f9149b4a9
AnghelLeonard <leoprivacy@yahoo.com>
2016-07-15 16:23:47 +0300

    Spring @Resource annoatation auto-wiring by type

---
+++ SpringResourceAnnotationAutowiringByType/src/main/java/org/spring/hello/HelloWorld.java
@@ -0,0 +1,34 @@
+package org.spring.hello;
+
+import javax.annotation.Resource;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+public class HelloWorld {
+
+    private String name;
+
+    // HelloWorld has a dependency on WorldCountries
+    @Resource   // @Resource annotation can also be used on bean property setter methods
+    private WorldCountries countries;
+
+    public HelloWorld() {
+        System.out.println("HelloWorld constructor invoked..");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{" + "name=" + name + ", " + countries + "}";
+    }
+
+}
