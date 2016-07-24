b38fbf27e9b42970c199a518c710ed448bee9953
AnghelLeonard <leoprivacy@yahoo.com>
2016-07-24 18:36:13 +0300

    Spring annotation based 1

---
+++ SpringAnnotationBasedHelloWorld_1/src/main/java/org/spring/MainApp.java
@@ -0,0 +1,26 @@
+package org.spring;
+
+import org.spring.bean.HelloWorld;
+import org.spring.config.MySpringConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(MySpringConfig.class);
+
+        HelloWorld obj = (HelloWorld) context.getBean(HelloWorld.class);
+        obj.printHello();
+
+        HelloWorld obj2 = (HelloWorld) context.getBean(HelloWorld.class);
+        obj2.setName("world");
+        obj2.printHello();
+    }
+
+}
