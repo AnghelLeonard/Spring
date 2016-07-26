6affe0103e5094521fa1439c9330d8013012a064
AnghelLeonard <leoprivacy@yahoo.com>
2016-07-26 11:51:25 +0300

    Asserting the SQL statement count generated by Hibernate JPA via Spring 4 MVC

--- AssertSQLStatementCount/src/main/java/sh/service/ProductServiceImpl.java
+++ AssertSQLStatementCount/src/main/java/sh/service/ProductServiceImpl.java
@@ -3,12 +3,14 @@ package sh.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import sh.dao.ProductRepository;
+import org.springframework.stereotype.Repository;
 import sh.model.Product;
 
 /**
  *
  * @author Anghel Leonard
  */
+@Repository
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
